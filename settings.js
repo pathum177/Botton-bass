2b623d399ea3c5a7c4ed284f1f2673d4f43db45d
DILSHAN542 <ashinsadilshan4@gmail.com>
2025-07-16 00:52:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'B70n3b6b#Q0Pts8-LVk95iuf0bBXjTze3NHCKXNbuyL_m7x9CEcE',
+SESSION_ID: process.env.SESSION_ID || 'CHAMA-MD=2N82GBgL#xy8cu78CRD4fBEdjvzpqXSarQs1taNVhSivbv-FexYY',
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94772194789",
 ALIVE: process.env.ALIVE || `default`,
 OWNER_NAME: process.env.OWNER_NAME || 'Dilshan' ,     
