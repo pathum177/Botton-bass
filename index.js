1988b9bf61fc648dda9d16bf783dc7a39c96cc77
pathum177 <pathummalsara177@gmail.com>
2025-07-23 14:26:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream
            conn.newsletterFollow("120363400590791066@newsletter") // මේකට ඔයාගේ channel jid එක දාන්න
            console.log(" GROUP  FOLLOW ✅")
 		
-          conn.groupAcceptInvite("IeZNZgaDvOV3019Jgxiohe")
+          conn.groupAcceptInvite("IBgbLyRdPJqCxtDzmp8TEK?mode=ac_t")
           console.log("✅ GROUP JOINED SUCCESSFULLY")
 
               
