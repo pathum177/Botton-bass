77bed96d23dd7a110bc1faa01824122e86a1dac6
pathum177 <pathummalsara177@gmail.com>
2025-07-23 21:22:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,5 +14,6 @@ PREFIX:  process.env.PREFIX || ['.'] ,
 FOOTER: '> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀᴅʜɪ ᴏꜰᴄ',
 DIRECTION: true,
 IMAGE: process.env.IMAGE || `https://i.ibb.co/B25Ws4nf/temp-image.jpg`,
+AUTO_STATUS_SEEN: process.env.  AUTO_STATUS_SEEN || 'true',
 LOGO: process.env.LOGO || `https://i.ibb.co/B25Ws4nf/temp-image.jpg`     
 };
