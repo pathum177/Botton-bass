12d4e159fdf003454ad662170bef2663e2ad0934
pathum177 <pathummalsara177@gmail.com>
2025-07-28 14:37:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'Jal1HTjQ#BxjUIvaZehRy3kL8IF2MaDcc4zPkT0I5uqQiXfM-zw',
+SESSION_ID: process.env.SESSION_ID || 'IK~Jal1HTjQ#BxjUIvaZehRy3kL8IF2MaDcc4zPkT0I5uqQiXfM-zw',
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94772194789",
 ALIVE: process.env.ALIVE || `default`,
 OWNER_NAME: process.env.OWNER_NAME || 'Dilshan' ,     
