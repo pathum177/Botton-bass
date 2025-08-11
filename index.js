b61139711a2f5f3f13c5ec56b2c6aff319aa7b6e
pathum177 <pathummalsara177@gmail.com>
2025-08-11 15:06:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const ownerNumber = '94772194789'
 //===================SESSION============================
 if (!fs.existsSync(__dirname + '/lib/creds.json')) {
     if (config.SESSION_ID) {
-      const sessdata = config.SESSION_ID.replace("𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=", "")
+      const sessdata = config.SESSION_ID.replace("YASIYA-MD~", "")
       const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
       filer.download((err, data) => {
         if (err) throw err
